75522e71b1c6033f902b4cd5e0bb7819498e3ac2
Wiratmika <wiratmika@gmail.com>
2016-12-28 19:10:25 +0700

    Fulfill client's so damn many requests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,29 @@ $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
     });
 
+    $('.currency').maskMoney({
+        'prefix': 'Rp ',
+        'precision': '0',
+        'thousands': '.'
+    });
+
+    $('.cc').payment('formatCardNumber');
+
     $('#table-trigger').click(function() {
         $('#table-triggered').fadeIn();
-    })
+    });
+
+    $("input[name='transfer-type']").change(function(){
+        var selected = '.' + $(this).val() + '-options';
+        console.log($(selected));
+        $('.transfer-options').hide()
+        $(selected).show();
+    });
+
+    $("input[name='receiver']").change(function(){
+        var selected = '.' + $(this).val() + '-options';
+        console.log($(selected));
+        $('.receiver-options').hide()
+        $(selected).show();
+    });
 });
