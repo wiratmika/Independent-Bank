70d7b22fc462d23ce512f99a651f6c69ca8744eb
Wiratmika <wiratmika@gmail.com>
2016-12-28 19:12:59 +0700

    Align JS conventions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,14 +17,14 @@ $(document).ready(function() {
         $('#table-triggered').fadeIn();
     });
 
-    $("input[name='transfer-type']").change(function(){
+    $('input[name=\'transfer-type\']').change(function(){
         var selected = '.' + $(this).val() + '-options';
         console.log($(selected));
         $('.transfer-options').hide()
         $(selected).show();
     });
 
-    $("input[name='receiver']").change(function(){
+    $('input[name=\'receiver\']').change(function(){
         var selected = '.' + $(this).val() + '-options';
         console.log($(selected));
         $('.receiver-options').hide()
